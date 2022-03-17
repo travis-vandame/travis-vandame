3e733756cd45c94f7f45ab9d998d8453bc8d596b
Travis VanDame <travis.vandame@gmail.com>
2022-03-17 00:41:04 -0500

    Updating dist after build

--- dist/assets/AboutView.9ecb841f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.1c29d321.js";import{o as t,c as o,a as _}from"./vendor.4f7de575.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};

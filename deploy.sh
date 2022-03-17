0f72c96b5b2d4a7be080a7bf0ae5779245f985f0
Travis VanDame <travis.vandame@gmail.com>
2022-03-17 01:49:47 -0500

    Deploy work

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if deploying to a custom domain
+# if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:travis-vandame/travis-vandame.git main:gh-pages
+git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -

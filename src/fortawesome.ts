2aa0e17096a7a2775215534671e0366152ff9a5c
Travis VanDame <travis.vandame@gmail.com>
2022-03-20 04:02:44 -0500

    Moved fontawesome imports into own .ts file

---
+++ src/fortawesome.ts
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars, faEllipsisVertical, faGear, faHome, faMinimize } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGear)
+library.add(faBars)
+library.add(faLinkedin)
+library.add(faGithub)
+library.add(faMinimize)
+library.add(faEllipsisVertical)
+library.add(faHome)
+
+export default FontAwesomeIcon;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars, faEllipsisVertical, faGear, faHome, faMinimize } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-
-library.add(faGear)
-library.add(faBars)
-library.add(faLinkedin)
-library.add(faGithub)
-library.add(faMinimize)
-library.add(faEllipsisVertical)
-library.add(faHome)
-
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import FontAwesomeIcon from "./fortawesome"
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon)

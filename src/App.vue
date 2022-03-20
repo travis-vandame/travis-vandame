2d0f78824435fec9d36eb48f531fa8c79ff0b1c3
Travis VanDame <travis.vandame@gmail.com>
2022-03-20 04:04:33 -0500

    cleanup in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import TheToolBar from "./components/TheToolBar.vue";
 import TheNavigationDrawer from "./components/TheNavigationDrawer.vue";
 import TheFooter from "./components/TheFooter.vue";
 import BaseLayout from "./components/BaseLayout.vue";
@@ -10,7 +9,6 @@ import TvAppBarVue from "./components/TvAppBar/TvAppBar.vue";
 <template>
   <BaseLayout>
     <template v-slot:app-bar>
-      <!-- <TheToolBar /> -->
       <TvAppBarVue />
     </template>
     <template v-slot:navigation-drawer>

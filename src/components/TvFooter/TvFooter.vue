25c4f5652124fa6c191963508144741ec7358f42
Travis VanDame <travis.vandame@gmail.com>
2022-03-22 05:10:35 -0500

    refactor(TheFooter) now TvFooter

---
+++ src/components/TvFooter/TvFooter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  id?: string
+}>(), {
+  id: 'default_footer'
+})
+
+const emit = defineEmits<{
+  (event: 'change'): void
+}>()
+</script>
+
+<template>
+    {{ props.id }}
+</template>
+
+<style scoped></style>

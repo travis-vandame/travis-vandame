2d479b51cb59f1b47bd5a8636b5e7e68f04c678c
Travis VanDame <travis.vandame@gmail.com>
2022-03-25 11:41:17 -0500

    refactor(TvDisplay) css refactoring

---
+++ src/components/TvDisplay/TvDisplay.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useWindow } from '../../composables/window'
+
+const { windowHeight, windowWidth } = useWindow()
+const screenHeight = screen.height
+const screenWidth = screen.width
+</script>
+
+<template>
+  <div class="display-information">
+    <div class="window caption">
+      <span>
+        Window H: {{ windowHeight }} W: {{ windowWidth }}
+      </span>
+    </div>
+    <div class="screen caption">
+      <span>
+        Resolution {{ screenHeight }} x {{ screenWidth }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.caption {
+  font-size: 12px;
+  font-weight: normal;
+  letter-spacing: 0.4;
+}
+</style>

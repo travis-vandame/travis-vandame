4cdbac6fe81d01902b8c2552b1e552b933707020
Travis VanDame <travis.vandame@gmail.com>
2022-03-27 12:54:01 -0500

    Commit feat(TvCard)

---
+++ src/components/TvCard/TvCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  description: String
+})  
+</script>
+
+<template>
+    <div class="card">
+        <slot name="title">{{ props.title }}</slot>
+        <slot name="description">{{ props.description }}</slot>
+        <slot name="toolbar">Toolbar</slot>
+    </div>
+</template>
+
+<style scoped>
+@media (max-width: 600px) { }
+@media (max-width: 480px) { }
+</style>
